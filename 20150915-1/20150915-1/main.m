b82afdc0f3a57b82cb4f47ac03ffb1cb05f360f2
liuchuo <95323362@qq.com>
2015-09-15 23:16:14 +0800

    20150915-1

---
+++ 20150915-1/20150915-1/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  20150915-1
+//
+//  Created by 欣 陈 on 15/9/15.
+//  Copyright (c) 2015年 欣 陈. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+int main(int argc,const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Programming is fun!");
+    }
+    return 0;
+}
