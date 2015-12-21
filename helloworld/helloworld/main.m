b17b809bd10c8683e3110b9d4d51d4070830d6a5
陈欣 <95323362@qq.com>
2015-12-21 11:48:52 +0800

    helloworld

---
+++ helloworld/helloworld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by ChenXin on 15/12/21.
+//  Copyright © 2015年 ChenXin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
