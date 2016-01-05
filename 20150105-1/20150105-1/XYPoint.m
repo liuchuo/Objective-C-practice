b4cd07a5c4a956a4a3460b5682a38087f66c8407
陈欣 <95323362@qq.com>
2016-01-05 21:26:12 +0800

    XYPoint.m

---
+++ 20150105-1/20150105-1/XYPoint.m
@@ -0,0 +1,20 @@
+//
+//  XYPoint.m
+//  20150105-1
+//
+//  Created by ChenXin on 16/1/5.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+//XYPoint.m 实现文件
+#import "XYPoint.h"
+
+@implementation XYPoint
+
+@synthesize x, y;
+
+-(void) setX: (int) xVal andY: (int) yVal; {
+    x = xVal;
+    y = yVal;
+}
+@end
