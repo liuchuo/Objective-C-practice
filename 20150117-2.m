e9baa97b082bf971a10dda9343ec13d4418ba8bd
陈欣 <95323362@qq.com>
2016-01-17 14:29:02 +0800

    第14章 Foundation框架简介

---
+++ 20150117-2.m
@@ -0,0 +1,11 @@
+//第14章 Foundation框架简介
+框架是由许多类、方法、函数和文档按照一定的逻辑组织起来的集合，以使研发程序变得更容易
+为所有的程序开发奠定基础的框架称为Foundation框架，它允许使用一些基本对象，如数字和字符串，以及一些对象集合，如数组、字典和集合。
+其他功能包括处理日期和时间、自动化的内存管理、处理基础文件系统、存储（或归档）对象、处理几何数据结构（如点和长方形）
+
+Application Kit框架包含广泛的类和方法，它们用来开发交互式图形应用程序，使得开发文本、菜单、工具栏、表、文档、剪贴板和窗口之类的过程变得十分简便
+Cocoa 总的来说是指 Foundation 框架、 Application Kit 框架和名为 Core Data 的第三方框架。
+Cocoa Touch 是指Foundation、Core Data 和 UIKit框架
+
+Foundation框架文档 这个文档包含Foundation的所有类及其实现的所有方法和函数的描述
+MAC OS X在线参考库：http://developer.apple.com/library/mac/navigation/index.html
